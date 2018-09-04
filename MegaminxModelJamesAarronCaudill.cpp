0a611b0b0fcb1329239e9e87d306d1cb4db9dc80
Aarron Caudill <aarron@Aarrons-MacBook-Pro.local>
2018-09-04 14:12:05 -0400

    testing

--- MegaminxModelJamesAarronCaudill.cpp
+++ MegaminxModelJamesAarronCaudill.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+
 using namespace std;
 
 bool runModel = true;
 
+
+void CPr(int color);
+void printSide(int color);
+void run();
+void baseMegaminx();
+int changeSide();
 //This struct is going to hold a single side of the Megaminx, there will be 12 objects of this structure.
 // Each of the objects will represent one color on the megaminx
 // The Colors will always stay the same are not to be changed, this will be used in the main function to help change colors, and one of the movements.
@@ -86,21 +93,21 @@ struct megaminx {
 //This will build the base Megaminx. This will be used to initilize the Megaminx to a "Solved State"
 //There is probably a better way to do this however this is functional.
 void baseMegaminx() {
-	white = {0, 1, 2, 3, 4, 5, {0,5,1}, {0,1}, {0,1,2}, {0,5}, {0,2}, {0,4,5}, {0,2,3}, {0,4}, {0,3}, {0,3,4}}
-	blue = {1, 0, 5, 9 ,10, 2, {1,2,0}, {1,0}, {1,0,5}, {1,2}, {1,5}, {1,9,2}, {1,5,10}, {1,9}, {1,10}, {1,10,9}}
-	yellow = {2, 0, 1, 10 ,11, 3, {2,3,0}, {2,0}, {2,0,1}, {2,3}, {2,1}, {2,8,3}, {2,1,9}, {2,8}, {2,9}, {2,9,8}}
-  purple = {3, 0, 2, 11 ,7, 4, {3,4,0}, {3,0}, {3,0,2}, {3,4}, {3,2}, {3,7,4}, {3,2,8}, {3,7}, {3,8}, {3,8,7}}
-	green = {4, 0, 3, 7 ,8, 5, {4,5,0}, {4,0}, {4,0,3}, {4,5}, {4,3}, {4,11,5}, {4,3,7}, {4,11}, {4,7}, {4,7,11}}
-	red = {5, 0, 4, 8, 9, 1, {5,1,0}, {5,0}, {5,0,4}, {5,1}, {5,4}, {5,10,1}, {5,4,11}, {5,10}, {5,11}, {5,11,10}}
-  grey = {6, 7, 8, 9, 10, 11, {6,11,7}, {6,7}, {6,7,8}, {6,11}, {6,8}, {6,10,11}, {6,8,9}, {6,10}, {6,9}, {6,9,10}}
-	lBlue = {7, 6, 11, 4, 3, 8, {7,8,6}, {7,6}, {7,6,11}, {7,8}, {7,11}, {7,3,8}, {7,11,4}, {7,3}, {7,4}, {7,4,3}}
-	orange = {8, 6, 7, 3, 2, 9, {8,9,6}, {8,6}, {8,6,7}, {8,9}, {8,7}, {8,2,9}, {8,7,3}, {8,2}, {8,3}, {8,3,2}}
-  lGreen = {9, 6, 8, 2, 1, 10, {9,10,6}, {9,6}, {9,6,8}, {9,10}, {9,8}, {9,1,10}, {9,8,2}, {9,1}, {9,2}, {9,2,1}}
-  pink = {10, 6, 9, 1, 5, 11, {10,11,6}, {10,6}, {10,6,9}, {10,11}, {10,8}, {10,5,11}, {10,9,1}, {10,5}, {10,1}, {10,1,5}}
-	lYellow = {11, 6, 10, 5, 4, 7, {11,7,6}, {11,6}, {11,6,10}, {11,7}, {11,10}, {11,4,7}, {11,10,5}, {11,4}, {11,5}, {11,5,4}}
+	megaminx white = {0, 1, 2, 3, 4, 5, {0,5,1}, {0,1}, {0,1,2}, {0,5}, {0,2}, {0,4,5}, {0,2,3}, {0,4}, {0,3}, {0,3,4}};
+	megaminx blue = {1, 0, 5, 9 ,10, 2, {1,2,0}, {1,0}, {1,0,5}, {1,2}, {1,5}, {1,9,2}, {1,5,10}, {1,9}, {1,10}, {1,10,9}};
+	megaminx yellow = {2, 0, 1, 10 ,11, 3, {2,3,0}, {2,0}, {2,0,1}, {2,3}, {2,1}, {2,8,3}, {2,1,9}, {2,8}, {2,9}, {2,9,8}};
+  megaminx purple = {3, 0, 2, 11 ,7, 4, {3,4,0}, {3,0}, {3,0,2}, {3,4}, {3,2}, {3,7,4}, {3,2,8}, {3,7}, {3,8}, {3,8,7}};
+	megaminx green = {4, 0, 3, 7 ,8, 5, {4,5,0}, {4,0}, {4,0,3}, {4,5}, {4,3}, {4,11,5}, {4,3,7}, {4,11}, {4,7}, {4,7,11}};
+	megaminx red = {5, 0, 4, 8, 9, 1, {5,1,0}, {5,0}, {5,0,4}, {5,1}, {5,4}, {5,10,1}, {5,4,11}, {5,10}, {5,11}, {5,11,10}};
+  megaminx grey = {6, 7, 8, 9, 10, 11, {6,11,7}, {6,7}, {6,7,8}, {6,11}, {6,8}, {6,10,11}, {6,8,9}, {6,10}, {6,9}, {6,9,10}};
+	megaminx lBlue = {7, 6, 11, 4, 3, 8, {7,8,6}, {7,6}, {7,6,11}, {7,8}, {7,11}, {7,3,8}, {7,11,4}, {7,3}, {7,4}, {7,4,3}};
+	megaminx orange = {8, 6, 7, 3, 2, 9, {8,9,6}, {8,6}, {8,6,7}, {8,9}, {8,7}, {8,2,9}, {8,7,3}, {8,2}, {8,3}, {8,3,2}};
+  megaminx lGreen = {9, 6, 8, 2, 1, 10, {9,10,6}, {9,6}, {9,6,8}, {9,10}, {9,8}, {9,1,10}, {9,8,2}, {9,1}, {9,2}, {9,2,1}};
+  megaminx pink = {10, 6, 9, 1, 5, 11, {10,11,6}, {10,6}, {10,6,9}, {10,11}, {10,8}, {10,5,11}, {10,9,1}, {10,5}, {10,1}, {10,1,5}};
+	megaminx lYellow = {11, 6, 10, 5, 4, 7, {11,7,6}, {11,6}, {11,6,10}, {11,7}, {11,10}, {11,4,7}, {11,10,5}, {11,4}, {11,5}, {11,5,4}};
 
 }
-
+/*
 void clockWise(int faceColor) {
 
 }
@@ -108,18 +115,19 @@ void clockWise(int faceColor) {
 void counterClockWise(int faceColor) {
 
 }
-void randomizer(randomMoves)
+void randomizer(int randomMoves)
 {
-	cout << "Welcome to the randomizer! You have " << randomMoves << " moves to make."
+	cout << "Welcome to the randomizer! You have " << randomMoves << " moves to make.";
 }
+*/
 
 void run(){
 	//Printing the base Megaminx using the white face
 	printSide(0);
 
 	while (runModel == true) {
-		int side = changeSide();
-		printSide(side)
+		//int side = changeSide();
+		//printSide(side);
 	}
 
 //TODO: This needs to be updated after the test run has been established that i can correctly change sides.
@@ -142,51 +150,52 @@ void run(){
 	}
 	*/
 }
-
+/*
 int changeSide(){
 	int side;
-	cout << "Change the sides of the MegaMinx: "
-	cin >> side;
+	//cout << "Change the sides of the MegaMinx: ";
+	//cin >> side;
 	return side;
 }
+*/
 void printSide(int color) {
-	string colorName;
+	megaminx colorName;
 	switch(color) {
 						//White will be Represented with int == 0
-						case 0 : colorName = 'white'; break;
+						case 0 : colorName = white; break;
 						//Blue will be Represented with int == 1
-						case 1 : colorName = 'blue'; break;
+						case 1 : colorName = blue; break;
 						//Yellow will be Represented with int == 2
-						case 2 : colorName = 'yellow'; break;
+						case 2 : colorName = yellow; break;
 						//Purple will be Represented with int == 3
-						case 3 : colorName = 'purple'; break;
+						case 3 : colorName = purple; break;
 						//Green will be Represented with int == 4
-						case 4 : colorName = 'green'; break;
+						case 4 : colorName = green; break;
 						//Red will be Represented with int == 5
-						case 5 : colorName = 'red'; break;
+						case 5 : colorName = red; break;
 						//Grey will be Represented with int == 6
-						case 6 : colorName = 'grey'; break;
+						case 6 : colorName = grey; break;
 						//Light Blue will be Represented with int == 7
-						case 7 : colorName = 'lblue'; break;
+						case 7 : colorName = lBlue; break;
 						//Light Yellow will be Represented with int == 8
-						case 8 : colorName = 'lyellow'; break;
+						case 8 : colorName = lYellow; break;
 						//Pink will be Represented with int == 9
-						case 9 : colorName = 'pink'; break;
+						case 9 : colorName = pink; break;
 						//Light Green will be Represented with int == 10
-						case 10 : colorName = 'lgreen'; break;
+						case 10 : colorName = lGreen; break;
 						//Orange will be Represented with int == 11
-						case 11 : colorName = 'orange'; break;
+						case 11 : colorName = orange; break;
 				}
 
-	printE = {colorName.bottomMiddle[0], colorName.bottomRightMiddle[0], colorName.topRightMiddle[0], colorName.topLeftMiddle[0], colorName.bottomLeftMiddle[0]};
-	printC = {colorName.bottomRight[0], colorName.topRight[0], colorName.top[0], colorName.topLeft[0], colorName.bototmLeft[0]};
-	printS = {colorName.bottomColor, colorName.bottomRightColor, colorName.topRightColor, colorName.topLeftColor, colorName.bottomLeftColor};
-	center = colorName.centerColor;
+	//int printE[5]; //contains the colours of the five edges in COUNTERCLOCKWISE order
+	//int printC[5]; //contains the colours of the five corners in COUNTERCLOCKWISE order
+	//int printS[5]; //contains the colours of the five adjacent sides in COUNTERCLOCKWISE order
+	//int center; //the colour of the center of the side to be printed
 
-	int printE[5]; //contains the colours of the five edges in COUNTERCLOCKWISE order
-	int printC[5]; //contains the colours of the five corners in COUNTERCLOCKWISE order
-	int printS[5]; //contains the colours of the five adjacent sides in COUNTERCLOCKWISE order
-	int center; //the colour of the center of the side to be printed
+	int printE[5] = {colorName.bottomMiddle[0], colorName.bottomRightMiddle[0], colorName.topRightMiddle[0], colorName.topLeftMiddle[0], colorName.bottomLeftMiddle[0]};
+	int printC[5] = {colorName.bottomRight[0], colorName.topRight[0], colorName.top[0], colorName.topLeft[0], colorName.bottomLeft[0]};
+	int printS[5] = {colorName.bottomColor, colorName.bottomRightColor, colorName.topRightColor, colorName.topLeftColor, colorName.bottomLeftColor};
+	int center = colorName.centerColor;
 
 	/*
 	This is where you need to populate the three arrays abouve with the correct colours
@@ -208,13 +217,15 @@ void printSide(int color) {
 	//top. Each line has several lines of code on it. Each line alternates between printing
 	//whitespace and calling a helper function to print the colour. Each line ends with
 	//cout << endl; which moves on to the next line. This is not very clean, but it works.
+	/*
 	cout << " "; CPr(printS[3]); cout << "    "; CPr(printC[2]); cout << "    "; CPr(printS[2]); cout << endl;
         cout << "    "; CPr(printE[3]); cout << "   "; CPr(printE[2]); cout << endl;
         cout << "  "; CPr(printC[3]); cout << "       "; CPr(printC[1]); cout << endl;
-        CPr(printS[4]); cout << " "; CPr(printE[4]); cout << "   "; CPr(rSide->colour); cout << "   "; CPr(printE[1]); cout << " "; CPr(printS[1]); cout << endl;
+        CPr(printS[4]); cout << " "; CPr(printE[4]); cout << "   "; CPr(center); cout << "   "; CPr(printE[1]); cout << " "; CPr(printS[1]); cout << endl;
         cout << "  "; CPr(printC[4]); cout << "   "; CPr(printE[0]); cout << "   "; CPr(printC[0]); cout << endl;
         cout << "      "; CPr(printS[0]); cout << endl << endl;
         return;
+				*/
 }
 
 //This next function is a helper function that prints a specific colour when given an integer
@@ -224,8 +235,8 @@ void printSide(int color) {
 //More info at en.wikipedia.org/wiki/ANSI_escape_code
 //You need to replace each colour's name with the number you use for the colour, or dont, if
 //you used enum with these colour names
-void CPr(int colour) {
-	switch(colour) {
+void CPr(int color) {
+	switch(color) {
 						//White will be Represented with int == 0
 		  			case 0 : cout << "\033[1;38;5;255mW\033[0m"; break;
 						//Blue will be Represented with int == 1
@@ -256,13 +267,13 @@ void CPr(int colour) {
 
 
 int main(){
-	cout << "Hello! Welcome to the Megaminx Model." << endl;
-	cout << "Please be patient while be build your Megaminx" << endl;
-	cout << "..." << endl;
+	//cout << "Hello! Welcome to the Megaminx Model." << endl;
+	//cout << "Please be patient while be build your Megaminx" << endl;
+	//cout << "..." << endl;
 
 	baseMegaminx();
 
-	cout << "Done!" << endl;
-	cout << "This is where you will make your moves." << endl;
-	run();
+	//cout << "Done!" << endl;
+	//cout << "This is where you will make your moves." << endl;
+	//run();
 }
